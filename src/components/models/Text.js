a494c839cf591ca404fe23d1680a8d5dd5fff8f5
jonah0502 <jonah0502@gmail.com>
2021-10-15 15:45:48 -0700

    dumb update

--- src/components/models/Text.js
+++ src/components/models/Text.js
@@ -22,7 +22,6 @@ export default function Text({ children, vAlign = 'center', hAlign = 'center', s
     mesh.current.position.y = (vAlign === 'center') ? -size.y / 2 : vAlign === 'top' ? 0 : -size.y
   }, [children])
 
-  const tGeo = new TextGeometry("JONAH", config)
 
   return (
     <mesh scale = {0.09} rotation = {[0, Math.PI /2, 0]} position={[0,1,0.5]}>
